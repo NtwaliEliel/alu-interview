bfc0bcdcd9022b849f746f54cbb4b85bd0feee14
NtwaliEliel <n.eliel1@alustudent.com>
2024-09-27 20:55:46 +0200

    Removing Indetations

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -7,11 +7,11 @@
 def rain(walls):
     if not walls:
         return 0
-    
+
     left, right = 0, len(walls) - 1
     left_max, right_max = walls[left], walls[right]
     water_trapped = 0
-    
+
     while left < right:
         if walls[left] < walls[right]:
             if walls[left] >= left_max:
@@ -25,5 +25,5 @@ def rain(walls):
             else:
                 water_trapped += right_max - walls[right]
             right -= 1
-    
+
     return water_trapped
